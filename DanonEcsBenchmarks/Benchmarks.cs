6aed562157acfbcce1eb7fabf7e46f3011448c95
Danon5 <danonfive@gmail.com>
2023-05-19 07:48:27 -0700

    - Initial commit

---
+++ DanonEcsBenchmarks/Benchmarks.cs
@@ -0,0 +1,86 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using DanonEcs;
+
+namespace DanonEcsBenchmarks {
+    [MemoryDiagnoser]
+    public class EcsBenchmarks {
+        private const int c_iteration_count = 16000;
+        private World m_world;
+
+        [GlobalSetup]
+        public void GlobalSetup() {
+            m_world = World.Create();
+        }
+
+        [GlobalCleanup]
+        public void GlobalCleanup() {
+            m_world.Destroy();
+        }
+
+        [IterationCleanup]
+        public void IterationCleanup() {
+            m_world.DestroyAllEntities();
+        }
+
+        [Benchmark]
+        public void BasicTest() {
+            for (var i = 0; i < c_iteration_count; i++) {
+                var entity = m_world.CreateEntity();
+                entity.Set<HealthComponent>();
+                entity.Set<ManaComponent>();
+            }
+        }
+
+        //[Benchmark]
+        public void AllocEntities() {
+            for (var i = 0; i < c_iteration_count; i++)
+                m_world.CreateEntity();
+        }
+
+        //[Benchmark]
+        public void BasicComponentOperations() {
+            for (var i = 0; i < c_iteration_count; i++) {
+                var entity = m_world.CreateEntity();
+                entity.Set<ExampleComponent>();
+                if (entity.Has<ExampleComponent>())
+                    entity.Remove<ExampleComponent>();
+            }
+        }
+
+        //[Benchmark]
+        public void AllocAndDestroyEntities() {
+            for (var i = 0; i < c_iteration_count; i++) {
+                var entity = m_world.CreateEntity();
+                entity.Destroy();
+            }
+        }
+
+        //[Benchmark]
+        public void AllocAndSetDataAndDestroyEntities() {
+            for (var i = 0; i < c_iteration_count; i++) {
+                var entity = m_world.CreateEntity();
+                entity.Set<ExampleComponent>();
+                entity.Destroy();
+            }
+        }
+
+        private struct ExampleComponent {
+            public int someData;
+        }
+
+        private struct HealthComponent {
+            public byte someData;
+        }
+
+        private struct ManaComponent {
+            public byte someData;
+        }
+    }
+
+    internal static class Benchmarks {
+        public static void Main(string[] args) {
+            BenchmarkRunner.Run<EcsBenchmarks>();
+        }
+    }
+}
---
+++ DanonEcsTests/Tests.cs
@@ -0,0 +1,124 @@
+﻿using DanonEcs;
+using NUnit.Framework;
+
+namespace DanonEcsTests {
+    [TestFixture]
+    public class Tests {
+        private World m_world;
+
+        [SetUp]
+        public void SetUp() {
+            m_world = World.Create();
+        }
+
+        [TearDown]
+        public void TearDown() {
+            m_world.Destroy();
+        }
+
+        [Test]
+        public void AddComponentTest() {
+            var entity = m_world.CreateEntity();
+            entity.Set<ExampleComponent>();
+            Assert.IsTrue(entity.Has<ExampleComponent>());
+        }
+
+        [Test]
+        public void SetComponentDataTest() {
+            var entity = m_world.CreateEntity();
+            entity.Set(new ExampleComponent {
+                someData = 5
+            });
+            Assert.AreEqual(entity.Get<ExampleComponent>().someData, 5);
+        }
+
+        [Test]
+        public void RemoveComponentTest() {
+            var entity = m_world.CreateEntity();
+            entity.Set<ExampleComponent>();
+            entity.Remove<ExampleComponent>();
+            Assert.IsFalse(entity.Has<ExampleComponent>());
+        }
+
+        [Test]
+        public void BasicMultiWorldTest() {
+            var world1 = World.Create();
+            var world2 = World.Create();
+
+            var entity1 = world1.CreateEntity();
+            Assert.IsFalse(entity1.Has<ExampleComponent>());
+            entity1.Set<ExampleComponent>();
+            Assert.IsTrue(entity1.Has<ExampleComponent>());
+
+            var entity2 = world2.CreateEntity();
+            Assert.IsFalse(entity2.Has<ExampleComponent>());
+            entity2.Set<ExampleComponent>();
+            Assert.IsTrue(entity2.Has<ExampleComponent>());
+
+            world1.Destroy();
+            world2.Destroy();
+        }
+
+        [Test]
+        public void ManyEntitiesTest() {
+            for (var i = 0; i < 10000; i++)
+                m_world.CreateEntity();
+        }
+
+        [Test]
+        public void DestroyEntitiesTest() {
+            for (var i = 0; i < 5; i++) {
+                var entity = m_world.CreateEntity();
+                entity.Set<ExampleComponent>();
+                entity.Destroy();
+            }
+
+            for (var i = 0; i < 5; i++) {
+                var entity = m_world.CreateEntity();
+                Assert.IsFalse(entity.Has<ExampleComponent>());
+            }
+
+            for (var i = 0; i < 5; i++) {
+                var entity = m_world.CreateEntity();
+                entity.Set<ExampleComponent>();
+            }
+        }
+
+        [Test]
+        public void DestroyWorldTest() {
+            var world = World.Create();
+
+            for (var i = 0; i < 5; i++) {
+                var entity = world.CreateEntity();
+                entity.Set<ExampleComponent>();
+            }
+
+            world.Destroy();
+        }
+
+        [Test]
+        public void EntityIsValidTest() {
+            var entity = m_world.CreateEntity();
+            Assert.IsTrue(entity.IsValid());
+            entity.Destroy();
+            Assert.IsFalse(entity.IsValid());
+            var entity2 = m_world.CreateEntity();
+            Assert.IsFalse(entity.IsValid());
+            Assert.IsTrue(entity2.IsValid());
+            entity2.Destroy();
+            Assert.IsFalse(entity2.IsValid());
+        }
+
+        [Test]
+        public void WorldIsValidTest() {
+            var world = World.Create();
+            Assert.IsTrue(world.IsValid());
+            world.Destroy();
+            Assert.IsFalse(world.IsValid());
+        }
+
+        private struct ExampleComponent {
+            public int someData;
+        }
+    }
+}
