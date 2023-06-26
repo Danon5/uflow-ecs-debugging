bfa59d3b10448e3be51ce802ce32596edbc4b3fd
Danon5 <danonfive@gmail.com>
2023-06-26 07:31:08 -0700

    - Made some minor tweaks

--- UFlowEcsBenchmarks/Benchmarks.cs
+++ UFlowEcsBenchmarks/Benchmarks.cs
@@ -1,44 +1,63 @@
 ﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Running;
 using UFlow.Addon.Ecs.Core.Runtime;
 
 namespace DanonEcsBenchmarks {
     [MemoryDiagnoser]
+    [Config(typeof(Config))]
+    [ShortRunJob]
     public class EcsBenchmarks {
-        private const int c_iteration_count = 16000;
+        private const int c_iteration_count = 1;
         private World m_world;
+        private DynamicEntitySet m_query;
+
+        private sealed class Config : ManualConfig {
+            public Config() {
+                AddColumn(new TagColumn("N", _ => c_iteration_count.ToString()));
+            }
+        }
 
         [IterationSetup]
         public void IterationSetup() {
             m_world = World.Create();
+            /*
+            m_query = m_world.CreateQuery().With<Health>().With<Mana>().AsSet();
+            for (var i = 1; i <= c_iteration_count; i++) {
+                var entity = m_world.CreateEntity();
+                entity.Set<Health>();
+                if (entity % 3 == 0)
+                    entity.Set<Mana>();
+            }
+            */
         }
 
         [IterationCleanup]
         public void IterationCleanup() {
             m_world.Destroy();
+            ExternalEngineEvents.clearStaticCachesEvent.Invoke();
         }
 
         [Benchmark]
-        public void CreateEntitiesWithComponents() {
-            for (var i = 0; i < c_iteration_count; i++) {
-                var entity = m_world.CreateEntity();
-                entity.Set<HealthComponent>();
-                entity.Set<ManaComponent>();
+        public void CreateQuery() {
+            var query = m_world.CreateQuery().With<Health>().AsSet();
+        }
+        
+        //[Benchmark]
+        public void IterateHealthManaQuery() {
+            foreach (var entity in m_query) {
+                ref var health = ref entity.Get<Health>();
+                ref var mana = ref entity.Get<Mana>();
             }
         }
 
-        [Benchmark]
-        public void QueryEntities() {
+        //[Benchmark]
+        public void CreateEntityWithHealthManaComponents() {
             for (var i = 0; i < c_iteration_count; i++) {
                 var entity = m_world.CreateEntity();
-                entity.Set<HealthComponent>();
-                entity.Set<ManaComponent>();
-            }
-
-            var query = m_world.CreateQuery().With<HealthComponent>().AsSet();
-            foreach (var entity in query) {
-                ref var health = ref entity.Get<HealthComponent>();
-                health.someData++;
+                entity.Set<Health>();
+                entity.Set<Mana>();
             }
         }
 
@@ -76,15 +95,15 @@ namespace DanonEcsBenchmarks {
         }
 
         private struct ExampleComponent {
-            public int someData;
+            public byte someData;
         }
 
-        private struct HealthComponent {
-            public int someData;
+        private struct Health {
+            public byte someData;
         }
 
-        private struct ManaComponent {
-            public int someData;
+        private struct Mana {
+            public byte someData;
         }
     }
 
